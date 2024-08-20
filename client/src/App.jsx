bfa01d8b3fd376a6e4caa82111e929b26996358b
Scott L <ehee27@gmail.com>
2024-08-20 09:27:34 -0500

    project files

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+      <p className="read-the-docs">Test App Running Scottie</p>
+    </>
+  )
+}
+
+export default App
